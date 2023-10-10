35ddd02c194f27c5d2397cd92b0b6201a461e66c
hachanghyun <haccwx@naver.com>
2023-10-10 22:10:25 +0900

    first commit

---
+++ src/main/java/com/hotong/VueExample/VueExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hotong.VueExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotong/VueExample/VueExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotong.VueExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
